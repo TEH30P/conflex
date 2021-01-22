f038a4f7d01b16791b391a5fd7ed6365c604c2fa
TEH3OP <TEH3OP@gmail.com>
2021-01-22 03:39:13 +0300

    Stable:   * no subvalues in list   * no inheritance   * no class stryle

--- conflex.py
+++ conflex.py
@@ -1,21 +1,6 @@
-from collections.abc import Mapping, Sequence
+from collections.abc import Mapping, Sequence, Set
 import copy as m_cp
 
-r'''
-#!!!TODO: build conf purpose
-self._l_node = {
-    section('main', {
-        'missing' << is_value(default='default'), 
-        'int' << is_value(),
-        'ints' << is_list(),
-        'complex' << is_value(attrs={'kind', 'type'}),
-    }),
-    section('ext', {
-        section('foo', {'a', 'b'}),
-        section('bar', {'a', 'b'})
-    })
-}
-'''
 
 def _opt_int_parse(iv: str) -> int:
     if type(iv) is not str:
@@ -38,53 +23,115 @@ def _opt_float_parse(iv: str) -> float:
     return float(iv)
 
 
-class OptAbc:
+def _opt_name_split(iv: str):
+    l_name = iv.split('_', maxsplit=1)
+    if len(l_name) == 2:
+        if l_name[0] not in ['s', 'v', 'l']:
+            return '', iv
+        else:
+            return l_name[0], l_name[1]
+    else:
+        return '', iv
+
+
+class DefOptAbc:
     def __init__(self, iv_kind: str):
-        self.kind = iv_kind
+        self.v_kind: str = iv_kind
+        self.v_name: str = ''
+        self.l_child: Set = set()
+
+    def name_set(self, iv: str):
+        if len(iv) == 0:
+            raise ValueError('Config option name must not to be empty.')
+
+        v_kind, v_name = _opt_name_split(iv)
+
+        if len([v for v in v_name if v.isspace() or v == '/']):
+            raise ValueError('Config option name must not contains `/` or space characters.')
+        if len(self.v_name):
+            raise ValueError('Name of option is already set it can not be updated.')
+        if len(v_kind) != 0 and v_kind != self.v_kind:
+            raise ValueError('Kind of option can not be updated.')
+
+        self.v_name = v_name
+
+    # self << iv
+    def __lshift__(self, iv):
+        if isinstance(iv, Set):
+            self.l_child = frozenset(iv)
+        else:
+            raise SyntaxError(
+                'Config option definition syntax is:'
+                '\n`option-name-as-string , ">>" , option-object [ , "<<" , "{" , child-options-as-set , "}" ]`')
+        return self
+
+    # iv >> self
+    def __rrshift__(self, iv):
+        if type(iv) is str:
+            self.name_set(iv)
+        else:
+            raise SyntaxError(
+                'Config option definition syntax is:'
+                '\n`option-name-as-string , ">>" , option-object [ , "<<" , "{" , child-options-as-set , "}" ]`')
+        return self
+
+    def __hash__(self):
+        return hash(self.v_name)
+
+    def __eq__(self, iv):
+        if isinstance(iv, str):
+            if iv == self.v_name:
+                raise KeyError(f'The option named `{self.v_name}` is already exists.')
+        elif isinstance(iv, DefOptAbc):
+            if iv.v_name == self.v_name:
+                raise KeyError(f'The option named `{self.v_name}` is already exists.')
+        else:
+            raise TypeError('Only plain strings `str` or derived from `DefOptAbc` objects allowed.')
+        return False
 
 
-class OptSection(OptAbc):
+class DefSection(DefOptAbc):
     def __init__(self):
-        super().__init__(iv_kind='s')
+        super().__init__('s')
 
 
-class OptItemAbc(OptAbc):
+class DefItemAbc(DefOptAbc):
     def __init__(self, iv_kind: str):
         super().__init__(iv_kind)
-        self.default = None
+        self.v_default = None
 
     def value_parse(self, iv):
         raise NotImplementedError('Method must be overridden.')
 
 
-class OptValue(OptItemAbc):
+class DefValue(DefItemAbc):
     def __init__(self, iv_default=None):
         super().__init__('v')
         if iv_default is None:
-            self.default = None
+            self.v_default = None
         elif type(iv_default) is not str and isinstance(iv_default, (Mapping, Sequence)):
-            raise AttributeError(r'Default value for `v` kind of options should be str, int, float, etc.')
+            raise TypeError(r'Default value for `v` kind of options should be str, int, float, etc.')
         else:
-            self.default = self.value_parse(iv_default)
+            self.v_default = self.value_parse(iv_default)
 
     def value_parse(self, iv):
         return iv
 
 
-class OptValueInt(OptValue):
+class DefValueInt(DefValue):
     def value_parse(self, iv: str) -> int:
         return _opt_int_parse(iv)
 
 
-class OptValueEnum(OptValue):
-    def __init__(self, il_mapping, iv_default=None):
+class DefValueEnum(DefValue):
+    def __init__(self, il_mapping: dict, iv_default=None):
         if isinstance(il_mapping, Mapping):
             self._l_mapping = il_mapping
         else:
             self._l_mapping = dict(il_mapping)
 
         if len(self._l_mapping) == 0:
-            raise AttributeError(r'Parameter il_mapping is empty.')
+            raise ValueError(r'Parameter il_mapping is empty.')
 
         super().__init__(iv_default)
 
@@ -92,18 +139,18 @@ class OptValueEnum(OptValue):
         return self._l_mapping[iv]
 
 
-class OptValueFloat(OptValue):
+class DefValueFloat(DefValue):
     def value_parse(self, iv: str) -> float:
         return _opt_float_parse(iv)
 
 
-class OptList(OptItemAbc):
+class DefList(DefItemAbc):
     def __init__(self, iv_default: list = None):
         super().__init__('l')
         if iv_default is None:
-            self.default = []
+            self.v_default = []
         else:
-            self.default = \
+            self.v_default = \
                 [self.value_parse(v)
                  for v in (iv_default if isinstance(iv_default, Sequence) else list(iv_default))]
 
@@ -111,18 +158,18 @@ class OptList(OptItemAbc):
         return iv
 
 
-class OptListInt(OptList):
+class DefListInt(DefList):
     def value_parse(self, iv: str) -> int:
         return _opt_int_parse(iv)
 
 
-class OptListFloat(OptList):
+class DefListFloat(DefList):
     def value_parse(self, iv: str) -> float:
         return _opt_float_parse(iv)
 
 
-class OptRdValue:
-    def __init__(self, iv_raw, iv_manager: OptValue):
+class OptValue:
+    def __init__(self, iv_raw, iv_manager: DefValue):
         self._manager = iv_manager
         self._raw = iv_raw
 
@@ -131,11 +178,11 @@ class OptRdValue:
         v = self._raw
         if isinstance(v, Mapping):
             v = v.get('v')
-        return self._manager.default if v is None else self._manager.value_parse(v)
+        return self._manager.v_default if v is None else self._manager.value_parse(v)
 
 
-class OptRdList:
-    def __init__(self, iv_raw, iv_manager: OptList):
+class OptList:
+    def __init__(self, iv_raw, iv_manager: DefList):
         self._manager = iv_manager
         self._raw = iv_raw
 
@@ -144,7 +191,7 @@ class OptRdList:
         v_raw = self._raw
 
         if v_raw is None:
-            v_raw = self._manager.default
+            v_raw = self._manager.v_default
         elif isinstance(v_raw, Mapping):
             v_raw = v_raw.get('l')
         return \
@@ -153,11 +200,49 @@ class OptRdList:
             else [self._manager.value_parse(v_raw)]
 
 
+def def_opt(iv_name: str) -> DefOptAbc:
+    v_kind, v_name = _opt_name_split(iv_name)
+
+    if v_kind in ['', 's']:
+        return v_name >> DefSection()
+    elif v_kind == 'v':
+        return v_name >> DefValue()
+    elif v_kind == 'l':
+        return v_name >> DefList()
+
+
 class Config(Mapping):
-    def __init__(self, il_parser: dict):
+    def __init__(self, il_parser: set):
         self._l_conf: dict = {}
-        self._l_parser = il_parser
+        self._l_parser = self._parser_dict_create(il_parser)
         self._v_iter = None
+
+    def _parser_dict_create(self, il_tree: set) -> dict:
+        """Create plain dict parser from input tree of opt definitions `DefOptAbc objects`.
+
+        :param il_tree: Set
+        :return: None
+        """
+
+        if not isinstance(il_tree, Set):
+            raise TypeError(f'{type(il_tree).__qualname__} is not `set` of `frozenset`')
+
+        l_l_root: list = [('', il_tree)]
+        l_plain: dict = {}
+
+        while len(l_l_root):
+            v_kp, l_root = l_l_root.pop()
+
+            for v_v in l_root:
+                if isinstance(v_v, DefOptAbc):
+                    if len(v_v.l_child):
+                        l_l_root.append((f'{v_kp}/{v_v.v_name}', v_v.l_child))
+                        v_v.l_child = frozenset()
+                    l_plain[f'{v_kp}/{v_v.v_name}'] = v_v
+                elif isinstance(v_v, str):
+                    l_plain[f'{v_kp}/{v_v}'] = def_opt(v_v)
+
+        return l_plain
 
     def _my_iter(self):
         if self._v_iter is None:
@@ -180,13 +265,13 @@ class Config(Mapping):
         v_key_part_len = len(l_key_part)
         if v_key_part_len == 2:
             if l_key_part[0] not in ['s', 'v', 'l']:
-                l_ret = (self._l_parser[f'{iv_parent_path}/{iv_raw}'].kind, iv_raw)
-            elif self._l_parser[f'{iv_parent_path}/{l_key_part[1]}'].kind != l_key_part[0]:
+                l_ret = (self._l_parser[f'{iv_parent_path}/{iv_raw}'].v_kind, iv_raw)
+            elif self._l_parser[f'{iv_parent_path}/{l_key_part[1]}'].v_kind != l_key_part[0]:
                 raise KeyError(r'Option exist but have different kind.')
             else:
                 l_ret = (l_key_part[0], l_key_part[1])
         elif v_key_part_len == 1:
-            l_ret = (self._l_parser[f'{iv_parent_path}/{iv_raw}'].kind, iv_raw)
+            l_ret = (self._l_parser[f'{iv_parent_path}/{iv_raw}'].v_kind, iv_raw)
         return l_ret
 
     def _value_get(self, iv_path: str):
@@ -214,11 +299,11 @@ class Config(Mapping):
         if len(v_pref) == 0:
             raise KeyError(r'Option path is empty.')
         elif v_pref == 's':
-            raise AttributeError('Sections can not have a value.')
+            raise TypeError('Sections can not have a value.')
         elif v_pref == 'v':
-            return OptRdValue(v_conf_opt, self._l_parser[v_path]).v
+            return OptValue(v_conf_opt, self._l_parser[v_path]).v
         elif v_pref == 'l':
-            return OptRdList(v_conf_opt, self._l_parser[v_path]).l
+            return OptList(v_conf_opt, self._l_parser[v_path]).l
 
     def load_d(self, il_raw_conf: dict) -> None:
         if isinstance(il_raw_conf, Mapping):
@@ -230,7 +315,7 @@ class Config(Mapping):
 class ConfigIter:
     def __init__(self, iv_parent: Config, il_option: dict):
         self._v_parent = iv_parent
-        self._l_path: list = [v_k for v_k, v_i in il_option.items() if v_i.kind != 's']
+        self._l_path: list = [v_k for v_k, v_i in il_option.items() if v_i.v_kind != 's']
         self._v_path_len = len(self._l_path)
         self._v_path_idx = -1
 
--- test/main.py
+++ test/main.py
@@ -24,33 +24,30 @@ def fv_conf_dict(request):
 def fv_conf_object(fv_conf_dict):
     class TestConf(m_c.Config):
         def __init__(self):
-            super(TestConf, self).__init__(
-                { '/main': m_c.OptSection()
-                , '/main/missing': m_c.OptValue(iv_default='default')
-                , '/main/lost': m_c.OptList(iv_default=list('default'))
-                , '/main/bool': m_c.OptValueEnum({'True': True, 'true': True, 'False': False, 'false': False})
-                , '/main/int': m_c.OptValueInt()
-                , '/main/bigint': m_c.OptValueInt()
-                , '/main/float': m_c.OptValueFloat()
-                , '/main/int_list': m_c.OptListInt()
-                , '/main/complex': m_c.OptValue()
-                , '/main/complex/kind': m_c.OptValue()}
-            )
-            # self._l_parser = \
-            #     { 'main':
-            #         { 'missing': m_c.OptValue(iv_default='default')
-            #         , 'lost': m_c.OptList(iv_default=list('default'))
-            #         , 'int': m_c.OptValue(if_value_parser=m_c.value_parser_int)
-            #         , 'bigint': m_c.OptValue(if_value_parser=m_c.value_parser_int)
-            #         , 'float': m_c.OptValue(if_value_parser=m_c.value_parser_float)
-            #         , 'int_list': m_c.OptList(if_item_value_parser=m_c.value_parser_int)
-            #         , 'complex': m_c.OptValue({
-            #             'kind': m_c.OptValue()
-            #             })
-            #         }
-            #     }
+            super(TestConf, self).__init__(set())
+            self._l_parser = \
+                { '/main': m_c.DefSection()
+                , '/main/missing': m_c.DefValue(iv_default='default')
+                , '/main/lost': m_c.DefList(iv_default=list('default'))
+                , '/main/bool': m_c.DefValueEnum({'True': True, 'true': True, 'False': False, 'false': False})
+                , '/main/int': m_c.DefValueInt()
+                , '/main/bigint': m_c.DefValueInt()
+                , '/main/float': m_c.DefValueFloat()
+                , '/main/int_list': m_c.DefListInt()
+                , '/main/complex': m_c.DefValue()
+                , '/main/complex/kind': m_c.DefValue()}
 
-    v_ret: TestConf = TestConf()
+    v_ret = m_c.Config({
+        'main' >> m_c.DefSection() << {
+            'missing' >> m_c.DefValue(iv_default='default'),
+            'lost' >> m_c.DefList(iv_default=list('default')),
+            'bool' >> m_c.DefValueEnum({'True': True, 'true': True, 'False': False, 'false': False}),
+            'int' >> m_c.DefValueInt(),
+            'bigint' >> m_c.DefValueInt(),
+            'float' >> m_c.DefValueFloat(),
+            'int_list' >> m_c.DefListInt(),
+            'complex' >> m_c.DefValue() << {
+                'kind' >> m_c.DefValue()}}})
     v_ret.load_d(fv_conf_dict)
     return v_ret
 
@@ -68,19 +65,19 @@ def test_config_values(fv_conf_object):
 
 
 def test_config_errors(fv_conf_object):
-    with pytest.raises(AttributeError):
+    with pytest.raises(TypeError) as x:
         v = fv_conf_object['/main']
-    with pytest.raises(KeyError):
+    with pytest.raises(KeyError) as x:
         v = fv_conf_object['main/int']
-    with pytest.raises(KeyError):
+    with pytest.raises(KeyError) as x:
         v = fv_conf_object['/main/dummy']
-    with pytest.raises(KeyError):
+    with pytest.raises(KeyError) as x:
         v = fv_conf_object['/main/l_int']
-    with pytest.raises(KeyError):
+    with pytest.raises(KeyError) as x:
         v = fv_conf_object['/main//int']
-    with pytest.raises(KeyError):
+    with pytest.raises(KeyError) as x:
         v = fv_conf_object['/main/z_int']
-    with pytest.raises(KeyError):
+    with pytest.raises(KeyError) as x:
         v = fv_conf_object['']
 
 
@@ -97,7 +94,7 @@ def test_config_kind_values(fv_conf_object):
 
 
 def test_config_kind_errors(fv_conf_object):
-    with pytest.raises(AttributeError):
+    with pytest.raises(TypeError):
         v = fv_conf_object['/s_main']
     with pytest.raises(KeyError):
         v = fv_conf_object['s_main/v_int']
@@ -112,116 +109,177 @@ def test_config_kind_errors(fv_conf_object):
     with pytest.raises(KeyError):
         v = fv_conf_object['']
 
+'''
 
+'''
 def test_config_iter(fv_conf_object):
-    l_option: list = \
-        [ ('/main/missing', 'default')
-        , ('/main/lost', list('default'))
-        , ('/main/bool', True)
-        , ('/main/int', 42)
-        , ('/main/bigint', 2 * 1024)
-        , ('/main/float', 3.141592)
-        , ('/main/int_list', [1, 2, 3])
-        , ('/main/complex', 'ok')
-        , ('/main/complex/kind', 'nice')]
+    l_option = \
+        { '/main/missing': 'default'
+        , '/main/lost': list('default')
+        , '/main/bool': True
+        , '/main/int': 42
+        , '/main/bigint': 2 * 1024
+        , '/main/float': 3.141592
+        , '/main/int_list': [1, 2, 3]
+        , '/main/complex': 'ok'
+        , '/main/complex/kind': 'nice'}
     v_idx = -1
-    for v_idx, l_option_container in enumerate(fv_conf_object):
-        assert (l_option_container[0], l_option_container[1]) == l_option[v_idx]
+    for v_option, v_content in fv_conf_object:
+        assert v_content == l_option[v_option]
+        v_idx += 1
     assert v_idx >= 0
     assert len(fv_conf_object) == len(l_option)
 
 
 def test_opt_manager_errors():
-    with pytest.raises(AttributeError):
-        v = m_c.OptValue(iv_default={})
     with pytest.raises(ValueError):
-        v = m_c.OptValueInt(iv_default='bad')
+        v = ' spaces ' >> m_c.DefValue()
     with pytest.raises(ValueError):
-        v = m_c.OptValueInt().value_parse('bad')
+        v = 'path/sep' >> m_c.DefValue()
     with pytest.raises(ValueError):
-        v = m_c.OptValueFloat(iv_default='bad')
+        m_c.DefValue().name_set('')
     with pytest.raises(ValueError):
-        v = m_c.OptValueInt().value_parse('bad')
-    with pytest.raises(AttributeError):
-        v = m_c.OptValueEnum({})
+        m_c.DefValue().name_set('l_bad')
+    with pytest.raises(ValueError):
+        v = m_c.DefValue()
+        v.name_set('main')
+        v.name_set('niam')
+    with pytest.raises(SyntaxError):
+        v = m_c.DefValue() << 'wrong_side'
+    with pytest.raises(SyntaxError):
+        v = {'wrong', 'side'} >> m_c.DefValue()
+    with pytest.raises(KeyError):
+        v = ('main' >> m_c.DefValue()) == ('main' >> m_c.DefValue())
+    with pytest.raises(KeyError):
+        v = ('main' == ('main' >> m_c.DefValue()))
+    with pytest.raises(TypeError):
+        v = (42 == m_c.DefValue())
+    with pytest.raises(TypeError):
+        v = (m_c.DefValue() == 42)
+    with pytest.raises(KeyError):
+        v = (('main' >> m_c.DefValue()) == 'main')
+    with pytest.raises(TypeError):
+        v = m_c.DefValue(iv_default={})
+    with pytest.raises(ValueError):
+        v = m_c.DefValueInt(iv_default='bad')
+    with pytest.raises(ValueError):
+        v = m_c.DefValueInt().value_parse('bad')
+    with pytest.raises(ValueError):
+        v = m_c.DefValueFloat(iv_default='bad')
+    with pytest.raises(ValueError):
+        v = m_c.DefValueInt().value_parse('bad')
+    with pytest.raises(ValueError):
+        v = m_c.DefValueEnum({})
 
 
 def test_opt_manager():
-    v = m_c.OptSection()
-    assert v.kind == 's'
+    v = m_c.def_opt('main')
+    assert v.v_name == 'main'
+    assert v.v_kind == 's'
+    assert type(v) == m_c.DefSection
 
-    v = m_c.OptValue(iv_default='ok')
-    assert v.kind == 'v'
-    assert v.default == 'ok'
+    v = m_c.def_opt('v_main')
+    assert v.v_name == 'main'
+    assert v.v_kind == 'v'
+    assert type(v) == m_c.DefValue
+
+    v = m_c.def_opt('l_main')
+    assert v.v_name == 'main'
+    assert v.v_kind == 'l'
+    assert type(v) == m_c.DefList
+
+    v = 'main' >> m_c.DefSection()
+    assert v.v_name == 'main'
+    assert v.v_kind == 's'
+    assert v != 'niam'
+
+    v = 'main' >> m_c.DefValue(iv_default='ok')
+    assert v.v_name == 'main'
+    assert v.v_kind == 'v'
+    assert v.v_default == 'ok'
     assert v.value_parse('ok') == 'ok'
 
-    v = m_c.OptValueInt(iv_default='1')
-    assert v.kind == 'v'
-    assert v.default == 1
+    v = 'main' >> m_c.DefValueInt(iv_default='1')
+    assert v.v_name == 'main'
+    assert v.v_kind == 'v'
+    assert v.v_default == 1
     assert v.value_parse('42') == 42
     assert v.value_parse('1KB') == 1024
 
-    v = m_c.OptValueFloat(iv_default='1')
-    assert v.default == 1.0
+    v = 'main' >> m_c.DefValueFloat(iv_default='1')
+    assert v.v_name == 'main'
+    assert v.v_default == 1.0
     assert v.value_parse('42') == 42.0
 
-    v = m_c.OptValueEnum(il_mapping={'Yes': 1, 'No': -1}, iv_default='Yes')
-    assert v.default == 1
+    v = 'main' >> m_c.DefValueEnum(il_mapping={'Yes': 1, 'No': -1}, iv_default='Yes')
+    assert v.v_name == 'main'
+    assert v.v_default == 1
     assert v.value_parse('No') == -1
     assert v.value_parse('Yes') == 1
 
-    v = m_c.OptValueEnum(il_mapping=[('Yes', 1), ('No', -1)], iv_default='Yes')
+    v = m_c.DefValueEnum(il_mapping=[('Yes', 1), ('No', -1)], iv_default='Yes')
     assert v.value_parse('No') == -1
     assert v.value_parse('Yes') == 1
 
-    v = m_c.OptList(iv_default=None)
-    assert v.kind == 'l'
-    assert v.default == []
+    v = 'main' >> m_c.DefList(iv_default=None)
+    assert v.v_name == 'main'
+    assert v.v_kind == 'l'
+    assert v.v_default == []
     assert v.value_parse('ok') == 'ok'
 
-    v = m_c.OptList(iv_default=[])
-    assert v.default == []
+    v = m_c.DefList(iv_default=[])
+    assert v.v_default == []
 
-    v = m_c.OptListInt(iv_default='42')
-    assert v.kind == 'l'
-    assert v.default == [4, 2]
+    v = 'main' >> m_c.DefListInt(iv_default='42')
+    assert v.v_name == 'main'
+    assert v.v_kind == 'l'
+    assert v.v_default == [4, 2]
     assert v.value_parse('42') == 42
     assert v.value_parse('1KB') == 1024
 
-    v = m_c.OptListInt(iv_default=range(0, 2))
-    assert v.default == [0, 1]
+    v = m_c.DefListInt(iv_default=range(0, 2))
+    assert v.v_default == [0, 1]
 
-    v = m_c.OptListInt(iv_default=[0, 1])
-    assert v.default == [0, 1]
+    v = m_c.DefListInt(iv_default=[0, 1])
+    assert v.v_default == [0, 1]
 
-    v = m_c.OptListFloat(iv_default='42')
-    assert v.kind == 'l'
-    assert v.default == [4.0, 2.0]
+    v = 'main' >> m_c.DefListFloat(iv_default='42')
+    assert v.v_name == 'main'
+    assert v.v_kind == 'l'
+    assert v.v_default == [4.0, 2.0]
     assert v.value_parse('42') == 42.0
 
-    v = m_c.OptListFloat(iv_default=range(0, 2))
-    assert v.default == [0.0, 1.0]
+    v = m_c.DefListFloat(iv_default=range(0, 2))
+    assert v.v_default == [0.0, 1.0]
 
-    v = m_c.OptListFloat(iv_default=[0, 1])
-    assert v.default == [0.0, 1.0]
+    v = m_c.DefListFloat(iv_default=[0, 1])
+    assert v.v_default == [0.0, 1.0]
 
-    v = m_c.OptListFloat(iv_default=[27.01, 16.09])
-    assert v.default == [27.01, 16.09]
+    v = m_c.DefListFloat(iv_default=[27.01, 16.09])
+    assert v.v_default == [27.01, 16.09]
 
 
-def test_conv_load():
-    v = m_c.Config({'a': m_c.OptValue(), 'b': m_c.OptValue()})
-    v.load_d({'a': 1, 'b': 2})
-    assert v._l_conf['a'] == 1
-    assert v._l_conf['b'] == 2
-    v.load_d([('a', 1), ('b', 2)])
-    assert v._l_conf['a'] == 1
-    assert v._l_conf['b'] == 2
+def test_conf_load():
+    v = m_c.Config({'s' >> m_c.DefSection() << {'v_a', 'v_b'}})
+    v.load_d({'s': {'a': 1, 'b': 2}})
+    assert v._l_conf['s']['a'] == 1
+    assert v._l_conf['s']['b'] == 2
+    v.load_d([('s', {'a': 1, 'b': 2})])
+    assert v._l_conf['s']['a'] == 1
+    assert v._l_conf['s']['b'] == 2
 
 
 def test_abc():
-    v = m_c.OptAbc('_')
-    assert v.kind == '_'
+    v = m_c.DefOptAbc('_')
+    assert v.v_kind == '_'
+    assert v.v_name == ''
+
+    v = 'main' >> m_c.DefOptAbc('_')
+    assert v.v_name == 'main'
 
     with pytest.raises(NotImplementedError):
-        v = m_c.OptItemAbc('_').value_parse('dummy')
+        v = m_c.DefItemAbc('_').value_parse('dummy')
+
+def test_conf_errors():
+    with pytest.raises(TypeError):
+        v = m_c.Config(42)
